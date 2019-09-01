7b9e055201283c7405638237f52e363960f19af3
kin911014 <50545361+kin911014@users.noreply.github.com>
2019-09-01 18:04:27 +0900

    Install babel and nodemon

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-console.log("hi!");
-n;
+import express from "express";
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("hello from home");
+
+const handleProfile = (req, res) => res.send("you are on my profile");
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
