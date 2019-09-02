0bc99d99cecf18e518909250a433903912025d5d
kin911014 <50545361+kin911014@users.noreply.github.com>
2019-09-02 14:33:55 +0900

    Install pug

--- routers/controller/videoController.js
+++ routers/controller/videoController.js
@@ -1,5 +1,5 @@
 export const searchCtr = (req, res) => res.send("search");
-export const homeCtr = (req, res) => res.send("home");
+export const homeCtr = (req, res) => res.render("home");
 export const videosCtr = (req, res) => res.send("videos");
 export const uploadCtr = (req, res) => res.send("upload");
 export const videoDetailCtr = (req, res) => res.send("videoDetail");
