d93e4ff8b1f89fc2c74449193c9b101e0564d540
kin911014 <50545361+kin911014@users.noreply.github.com>
2019-09-03 20:49:41 +0900

    add the function at the search.pug

--- routers/controller/userController.js
+++ routers/controller/userController.js
@@ -1,19 +1,19 @@
-export const joinCtr = (req, res) => res.render("join", { pageTitle: "Home" });
+export const joinCtr = (req, res) => res.render("join", { pageTitle: "Join" });
 
 export const loginCtr = (req, res) =>
   res.render("login", { pageTitle: "Login" });
 
 export const logoutCtr = (req, res) =>
-  res.render("logout", { pageTitle: "Home" });
+  res.render("logout", { pageTitle: "Logout" });
 
 export const usersCtr = (req, res) =>
-  res.render("users", { pageTitle: "Home" });
+  res.render("users", { pageTitle: "Users" });
 
 export const userDetailCtr = (req, res) =>
-  res.render("userDetail", { pageTitle: "Home" });
+  res.render("userDetail", { pageTitle: "User Detail" });
 
 export const editProfileCtr = (req, res) =>
-  res.render("editProfile", { pageTitle: "Home" });
+  res.render("editProfile", { pageTitle: "Edit Profile" });
 
 export const changePasswordCtr = (req, res) =>
-  res.render("changePassword", { pageTitle: "Home" });
+  res.render("changePassword", { pageTitle: "Change Password" });
--- routers/controller/videoController.js
+++ routers/controller/videoController.js
@@ -1,19 +1,23 @@
-export const searchCtr = (req, res) =>
-  res.render("search", { pageTitle: "Search" });
+export const searchCtr = (req, res) => {
+  const {
+    query: { term: searchingFor }
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingFor });
+};
 
 export const homeCtr = (req, res) => res.render("home", { pageTitle: "Home" });
 
 export const videosCtr = (req, res) =>
-  res.render("videos", { pageTitle: "Home" });
+  res.render("videos", { pageTitle: "Videos" });
 
 export const uploadCtr = (req, res) =>
-  res.render("upload", { pageTitle: "Home" });
+  res.render("upload", { pageTitle: "Upload" });
 
 export const videoDetailCtr = (req, res) =>
-  res.render("videoDetail", { pageTitle: "Home" });
+  res.render("videoDetail", { pageTitle: "Video Detail" });
 
 export const editVideoCtr = (req, res) =>
-  res.render("editVideo", { pageTitle: "Home" });
+  res.render("editVideo", { pageTitle: "Edit Video" });
 
 export const deleteVideoCtr = (req, res) =>
-  res.render("deleteVideo", { pageTitle: "Home" });
+  res.render("deleteVideo", { pageTitle: "Delete Video" });
