add5ed38f973f6f17ad19a0027aaf5c6271ec7ba
kin911014 <50545361+kin911014@users.noreply.github.com>
2019-09-14 20:10:21 +0900

    Created comment schema and model

--- models/Video.js
+++ models/Video.js
@@ -16,8 +16,16 @@ const VideoSchema = new mongoose.Schema({
   },
   createdAt: {
     type: Date,
-    default: Date.now()
-  }
+    default: Date.now
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ]
 });
 
 const model = mongoose.model("Video", VideoSchema);
+
+export default model;
