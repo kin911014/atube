c6a096fc3d964a58ce459f088b357bf51ca002ad
kin911014 <50545361+kin911014@users.noreply.github.com>
2019-10-09 01:11:55 +0900

    Adding creator to video

--- models/Comment.js
+++ models/Comment.js
@@ -8,7 +8,10 @@ const CommentSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now
-  }
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
 });
 
 const model = mongoose.model("Comment", CommentSchema);
