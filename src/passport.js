7a28acacb9a744a09eed44a1ea5eaf38f9e339cd
kin911014 <50545361+kin911014@users.noreply.github.com>
2019-11-04 00:08:30 +0900

    Modify passport

--- src/passport.js
+++ src/passport.js
@@ -25,7 +25,7 @@ passport.use(
     {
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
-      callbackURL: `http://localhost:4000${routes.githubCallback}`
+      callbackURL: `https://stark-caverns-84778.herokuapp.com${routes.githubCallback}`
     },
     githubLoginCallback
   )
